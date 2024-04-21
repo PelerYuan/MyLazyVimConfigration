d7b4f339623e0d86ed241a8a038d62c24f60b877
PelerYuan <pelerlittlepig@outlook.com>
2024-04-22 07:10:47 +0800

    telescope

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,189 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- This file is automatically loaded by lazyvim.config.init
+
+-- DO NOT USE THIS IN YOU OWN CONFIG!!
+-- use `vim.keymap.set` instead
+local map = vim.keymap.set
+
+-- better up/down
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+
+-- Move to window using the <ctrl> hjkl keys
+map("n", "<leader>h", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<leader>j", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<leader>k", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<leader>l", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
+-- Resize window using <ctrl> arrow keys
+map("n", "<C-k>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-j>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-l>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-h>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- Move Lines
+map("n", "<S-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
+map("n", "<S-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
+map("i", "<S-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+map("i", "<S-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map("v", "<S-j>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
+map("v", "<S-k>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
+
+-- buffers
+map("n", "<A-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "<A-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+map("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+map("n", "<A-e>", ":Bdelete!<CR>")
+
+-- Clear search with <esc>
+map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
+
+-- Clear search, diff update and redraw
+-- taken from runtime/lua/_editor.lua
+map(
+  "n",
+  "<leader>ur",
+  "<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>",
+  { desc = "Redraw / Clear hlsearch / Diff Update" }
+)
+
+-- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+map("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next Search Result" })
+map("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+map("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+map("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Prev Search Result" })
+map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+
+-- Add undo break-points
+map("i", ",", ",<c-g>u")
+map("i", ".", ".<c-g>u")
+map("i", ";", ";<c-g>u")
+
+-- save file
+map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+map({ "i", "x", "n", "s" }, "<A-w>", "<cmd>w<cr><esc>", { desc = "Save File" })
+
+--keywordprg
+map("n", "<leader>K", "<cmd>norm! K<cr>", { desc = "Keywordprg" })
+
+-- better indenting
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+
+-- lazy
+map("n", "<A-L>", "<cmd>Lazy<cr>", { desc = "Lazy" })
+
+-- new file
+map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
+
+map("n", "<leader>ll", "<cmd>lopen<cr>", { desc = "Location List" })
+map("n", "<leader>fl", "<cmd>copen<cr>", { desc = "Quickfix List" })
+
+map("n", "[q", vim.cmd.cprev, { desc = "Previous Quickfix" })
+map("n", "]q", vim.cmd.cnext, { desc = "Next Quickfix" })
+
+-- formatting
+map({ "n", "v" }, "<leader>cf", function()
+  LazyVim.format({ force = true })
+end, { desc = "Format" })
+
+-- diagnostic
+local diagnostic_goto = function(next, severity)
+  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+  severity = severity and vim.diagnostic.severity[severity] or nil
+  return function()
+    go({ severity = severity })
+  end
+end
+map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+map("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
+map("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
+map("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
+map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
+map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
+map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
+
+-- stylua: ignore start
+
+-- toggle options
+map("n", "<leader>sf", function() LazyVim.format.toggle() end, { desc = "Toggle Auto Format (Global)" })
+map("n", "<leader>sF", function() LazyVim.format.toggle(true) end, { desc = "Toggle Auto Format (Buffer)" })
+map("n", "<leader>ss", function() LazyVim.toggle("spell") end, { desc = "Toggle Spelling" })
+map("n", "<leader>sw", function() LazyVim.toggle("wrap") end, { desc = "Toggle Word Wrap" })
+map("n", "<leader>sL", function() LazyVim.toggle("relativenumber") end, { desc = "Toggle Relative Line Numbers" })
+map("n", "<leader>sl", function() LazyVim.toggle.number() end, { desc = "Toggle Line Numbers" })
+map("n", "<leader>sd", function() LazyVim.toggle.diagnostics() end, { desc = "Toggle Diagnostics" })
+local conceallevel = vim.o.conceallevel > 0 and vim.o.conceallevel or 3
+map("n", "<leader>sc", function() LazyVim.toggle("conceallevel", false, {0, conceallevel}) end, { desc = "Toggle Conceal" })
+if vim.lsp.buf.inlay_hint or vim.lsp.inlay_hint then
+  map( "n", "<leader>sh", function() LazyVim.toggle.inlay_hints() end, { desc = "Toggle Inlay Hints" })
+end
+map("n", "<leader>sT", function() if vim.b.ts_highlight then vim.treesitter.stop() else vim.treesitter.start() end end, { desc = "Toggle Treesitter Highlight" })
+map("n", "<leader>sb", function() LazyVim.toggle("background", false, {"light", "dark"}) end, { desc = "Toggle Background" })
+
+-- lazygit
+map("n", "<leader>gg", function() LazyVim.lazygit( { cwd = LazyVim.root.git() }) end, { desc = "Lazygit (Root Dir)" })
+map("n", "<leader>gG", function() LazyVim.lazygit() end, { desc = "Lazygit (cwd)" })
+-- map("n", "<leader>gb", LazyVim.lazygit.blameline, { desc = "Git Blame Line" })
+
+map("n", "<leader>gf", function()
+  local git_path = vim.api.nvim_buf_get_name(0)
+  LazyVim.lazygit({args = { "-f", vim.trim(git_path) }})
+end, { desc = "Lazygit Current File History" })
+
+-- quit
+map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
+
+-- highlights under cursor
+map("n", "<leader>si", vim.show_pos, { desc = "Inspect Pos" })
+
+-- LazyVim Changelog
+-- map("n", "<A-L>", function() LazyVim.news.changelog() end, { desc = "LazyVim Changelog" })
+
+-- floating terminal
+local lazyterm = function() LazyVim.terminal(nil, { cwd = LazyVim.root() }) end
+map("n", "<leader>ft", lazyterm, { desc = "Terminal (Root Dir)" })
+map("n", "<leader>fT", function() LazyVim.terminal() end, { desc = "Terminal (cwd)" })
+map("n", "<c-/>", lazyterm, { desc = "Terminal (Root Dir)" })
+map("n", "<c-_>", lazyterm, { desc = "which_key_ignore" })
+
+-- Terminal Mappings
+map("t", "jk", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
+map("t", "<leader>h", "<cmd>wincmd h<cr>", { desc = "Go to Left Window" })
+map("t", "<leader>j", "<cmd>wincmd j<cr>", { desc = "Go to Lower Window" })
+map("t", "<leader>k", "<cmd>wincmd k<cr>", { desc = "Go to Upper Window" })
+map("t", "<leader>l", "<cmd>wincmd l<cr>", { desc = "Go to Right Window" })
+map("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+map("t", "<c-_>", "<cmd>close<cr>", { desc = "which_key_ignore" })
+
+-- windows
+map("n", "<leader>wc", "<C-W>p", { desc = "Other Window", remap = true })
+map("n", "<leader>wd", "<C-W>c", { desc = "Delete Window", remap = true })
+map("n", "<leader>w-", "<C-W>s", { desc = "Split Window Below", remap = true })
+map("n", "<leader>w|", "<C-W>v", { desc = "Split Window Right", remap = true })
+map("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
+map("n", "<leader>|", "<C-W>v", { desc = "Split Window Right", remap = true })
+
+-- tabs
+map("n", "<leader>bl", "<cmd>tablast<cr>", { desc = "Last Tab" })
+map("n", "<leader>bf", "<cmd>tabfirst<cr>", { desc = "First Tab" })
+map("n", "<leader>bn", "<cmd>tabnew<cr>", { desc = "New Tab" })
+map("n", "<leader>bl", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+map("n", "<leader>bc", "<cmd>tabclose<cr>", { desc = "Close Tab" })
+map("n", "<leader>bp", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+
+-- jk
+map("i", "jk", "<ESC>")
+map("v", "jk", "<ESC>")
+
+-- exit & save
+map("n", "<A-w>", ":w<CR>", {desc = "file saved"})
+map("n", "<A-q>", ":q<CR>", {desc = "exited"})
+
+-- visual Mode
+map("n", "vv", "<C-v>")
